7d0ac5893666dda82350b6ea5498ed7565216496
YOGESSH V <yogesshnec@gmail.com>
2024-10-22 21:25:33 +0530

    Two Pointers Problem solving

---
+++ FAANG - 2025/Two Pointers/two_sum.py
@@ -0,0 +1,8 @@
+def twoSum(nums, target):
+        visitedIdx = {}
+        for idx in range(len(nums)):
+            difference = target - nums[idx]
+            if difference in visitedIdx:
+                return[visitedIdx[difference], idx]
+            visitedIdx[nums[idx]] = idx
+        return []
